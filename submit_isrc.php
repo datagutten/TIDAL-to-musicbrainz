1190fdb28dd21fa5f0cba0e2a7342a45685198f9
datagutten <datagutten@datagutten.net>
2017-07-21 22:14:21 +0200

    Added tool to submit ISRCs for a single album

---
+++ submit_isrc.php
@@ -0,0 +1,26 @@
+<?php	
+require '../musicbrainz/musicbrainz.class.php';
+$mb=new musicbrainz;
+require_once '../TIDALtools/tidalinfo.class.php';
+$tidal=new tidalinfo;
+
+if(empty($argv[1]) || empty($argv[2]))
+	die('Usage: batch_submit_isrc.php [release MBID] [TIDAL Album ID or URL]'."\n");
+
+$album_isrc=$tidal->album_isrc($argv[2]); //Fetch ISRCs for the album from TIDAL
+if($album_isrc===false)
+	die($tidal->error."\n");
+$release=$mb->getrelease($argv[1],'recordings');
+if($release===false)
+	die($mb->error."\n");
+$albuminfo=$tidal->album($argv[2]); //Get album info for verification
+if((string)$release->release->title!==$albuminfo['title'] && (empty($argv[3]) || $argv[3]!='ignore'))
+	die(sprintf("Titles does not match:\nTIDAL: %s\nMusicBrainz: %s\n",$albuminfo['title'],(string)$release->release->title));
+
+$isrc_list=$mb->build_isrc_list($album_isrc,$release);
+if($isrc_list===false)
+	die($mb->error."\n");
+
+$result=$mb->send_isrc_list($isrc_list);
+		
+echo $mb->error."\n";

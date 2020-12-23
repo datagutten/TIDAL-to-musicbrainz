6f6f009d2e53103918ec51a4489bf72965074a12
datagutten <datagutten@datagutten.net>
2020-12-23 21:22:26 +0100

    Add new method to submit ISRC from TIDAL

--- src/TIDAL_to_musicbrainz.php
+++ src/TIDAL_to_musicbrainz.php
@@ -32,42 +32,55 @@ class TIDAL_to_musicbrainz
         $this->version = InstalledVersions::getVersion('datagutten/tidal-to-musicbrainz');
 	}
 
+    /**
+     * Fetch ISRC from TIDAL and submit to MusicBrainz
+     * @param string $album_mbid Album MBID
+     * @param Tidal\elements\Album $tidal_album Tidal album object
+     * @param int $distance_tolerance Levenshtein distance tolerance for track titles
+     * @return array Response from MusicBrainz
+     * @throws TIDAL_to_musicbrainzException
+     * @throws musicbrainz\exceptions\MusicBrainzException
+     */
+    function submit_isrc_obj(string $album_mbid, Tidal\elements\Album $tidal_album, $distance_tolerance = 3)
+    {
+        $release = $this->mb->getrelease($album_mbid, 'recordings', true);
+        $isrc = [];
+        foreach ($release['media'] as $medium)
+        {
+            foreach ($medium['tracks'] as $track)
+            {
+                $tidal_track = $tidal_album->get_track($track['position'], $medium['position']);
+                if(empty($tidal_track->isrc))
+                    continue;
+                $check_tidal = mb_strtolower($tidal_track->title);
+                $check_mb = mb_strtolower($track['title']);
+                $distance = levenshtein($check_mb, $check_tidal);
+
+                if ($check_tidal !== $check_mb && $distance>$distance_tolerance)
+                {
+                    $msg = sprintf("Titles does not match:\nTIDAL: %s\nMusicBrainz: %s (Levenshtein distance %d)\n",
+                        $check_tidal, $check_mb, $distance);
+                    throw new TIDAL_to_musicbrainzException($msg);
+                }
+                $isrc[$track['recording']['id']] = $tidal_track->isrc;
+            }
+        }
+
+        $isrc_list = musicbrainz\musicbrainz::build_isrc_list_array($isrc);
+        return $this->mb->send_isrc_list($isrc_list, 'datagutten/tidal-to-musicbrainz-'.$this->version);
+    }
+
 	/**
 	 * Fetch ISRC from TIDAL and submit to MusicBrainz
 	 * @param string $album_mbid Album MBID
 	 * @param string $tidal_id Tidal ID or URL
 	 * @throws Exception
+     * @deprecated Use submit_isrc_obj
 	 * @return array
 	 */
-	function submit_isrc($album_mbid,$tidal_id)
+	function submit_isrc(string $album_mbid,string $tidal_id)
 	{
-		try {
-			$album_isrc=$this->tidal->album_isrc($tidal_id); //Fetch ISRCs for the album from TIDAL
-			$albuminfo=$this->tidal->album($tidal_id); //Get album info for verification
-		}
-		catch (Tidal\TidalError $e)
-		{
-			throw new Exception('Error fetching information from TIDAL: '. $e->getMessage());
-		}
-
-		try {
-			$release = $this->mb->getrelease($album_mbid, 'recordings');
-
-			if (strtolower((string)$release->{'release'}->{'title'}) !== strtolower($albuminfo['title']) && (empty($argv[3]) || $argv[3] != 'ignore'))
-				throw new Exception(sprintf("Titles does not match:\nTIDAL: %s\nMusicBrainz: %s\n",
-					$albuminfo['title'], (string)$release->{'release'}->{'title'}));
-
-			$isrc_list = $this->mb->build_isrc_list($album_isrc, $release);
-
-			return $this->mb->send_isrc_list($isrc_list);
-		}
-		catch (musicbrainz\exceptions\MusicBrainzException $e)
-		{
-			throw new Exception('Error from MusicBrainz: '.$e->getMessage());
-		}
-		catch (Requests_Exception $e)
-		{
-			throw new Exception('Error sending data using requests: '. $e->getMessage());
-		}
+        $album_obj = Album::from_tidal($tidal_id);
+        return $this->submit_isrc_obj($album_mbid, $album_obj);
 	}
 }

3415b1edf92e08057199165e7b4828c16c57d455
datagutten <datagutten@datagutten.net>
2021-08-06 15:51:39 +0200

    Rewrite batch_submit_isrc.php with new Tidal class

--- batch_submit_isrc.php
+++ batch_submit_isrc.php
@@ -7,9 +7,10 @@ use datagutten\tidal_musicbrainz\TIDAL_to_musicbrainz;
 require 'vendor/autoload.php';
 $mb=new musicbrainz\musicbrainz;
 $tidal_to_mb = new TIDAL_to_musicbrainz();
-$tidal=$info=$tidal_to_mb->tidal;
+$tidal = new Tidal\Search();
+
 try {
-	$info->token = Tidal\Info::get_token();
+    $tidal->token = Tidal\Info::get_token();
 }
 catch (Tidal\TidalError $e) {
 	die($e->getMessage());
@@ -20,18 +21,18 @@ if(empty($argv[1]))
 
 try {
     $releases = $mb->api_request(sprintf('/artist/%s?inc=releases', $argv[1]));
+    $artist = $mb->artistFromMBID($argv[1]);
 }
 catch (musicbrainz\exceptions\MusicBrainzException $e)
 {
     die('Error from MusicBrainz: '. $e->getMessage()."\n");
 }
 
-foreach($releases->artist->{'release-list'}->release as $release)
+foreach($artist->releases as $release)
 {
 	printf("MB release: %s\n",$release->title);
 	try {
-		$response = $info->query(sprintf('https://api.tidal.com/v1/search?query=%s&limit=3&offset=0&types=ALBUMS&countryCode=NO', urlencode($release->title)));
-		$results = $info->parse_response($response);
+		$results = $tidal->search_albums($release->title);
 	}
 	catch (Tidal\TidalError $e)
 	{
@@ -39,7 +40,7 @@ foreach($releases->artist->{'release-list'}->release as $release)
 		continue;
 	}
 
-	foreach($results['albums']['items'] as $album)
+	foreach($results->albums as $album)
 	{
 		echo "\t".$album['title']."\n";
 		if(!empty($release->barcode) && !empty($album['upc']))
@@ -59,15 +60,16 @@ foreach($releases->artist->{'release-list'}->release as $release)
 		//Missing barcode, check other fields
 		if((string)$release->title!==$album['title'])
 		{
-			echo "\tMatching ".(string)$release->title." by name\n";
+			//echo "\tMatching ".(string)$release->title." by name\n";
 			continue;
 		}
 	}
 	if(!empty($match))
 	{
-		echo "MB title: ".$release->{'title'}."\n";
+		echo "MB title: ".$release->title."\n";
 		try {
-            $tidal_to_mb->submit_isrc((string)$release->attributes()['id'], $album['id']);
+            $match->get_tracks();
+            $tidal_to_mb->submit_isrc_obj($release->id, $match);
         }
         catch (Exception $e)
         {
